57611de32494f98cc7aa75f806f3781bbfb60784
Renato <renatolopezflores3456@gmail.com>
2026-01-15 09:50:39 -0300

    Optimizar configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,29 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Reduce el tamaño de advertencias de chunks
+    chunkSizeWarningLimit: 1000,
+    // Optimiza el proceso de build
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        // Separa dependencias en chunks manuales
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+  // Optimiza las dependencias
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
+  // Previene errores de memoria
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
 })
