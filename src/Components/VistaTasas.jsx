0e325df1e432f4af933f04ab8c85788d56e35239
Fito3456 <121978264+Fito3456@users.noreply.github.com>
2026-01-15 11:51:37 -0300

    Update import path for storage utility

--- src/Components/VistaTasas.jsx
+++ src/Components/VistaTasas.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Edit2 } from 'lucide-react';
-import { storage } from '../utils/storage';
+import { storage } from './Utils/storage';
 
 const VistaTasas = ({ tasas, setTasas }) => {
   const campos = [
